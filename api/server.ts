a1ec12b442a8da6e92ac0c9902045e33cee103cf
Sumit <sumitpadalkar53@gmail.com>
2024-09-26 12:39:12 +0530

    setup:cors & nodemon

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,17 @@
 import express,{Express,Request,Response} from 'express'
+import cors from 'cors'
 import getSuccessMsg from './utils/reponses/successMsg'
-const app:Express= express()
+import eventRoutes from './routes/events/event.route'
 
+export const app:Express= express()
 const PORT= process.env.PORT||3000
 
+//middlewares
+app.use(cors())
+app.use("/api/v1/events",eventRoutes)
 
+
+//default route
 app.get("/",(req:Request,res:Response)=>{
     res.json(getSuccessMsg({
         success:true,
